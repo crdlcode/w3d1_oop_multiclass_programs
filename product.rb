713972bcd4c6b0ed449fea6ce4a758c370ac60ab
curtischatch <curtis.chat.hk@gmail.com>
2018-07-24 22:39:59 -0400

    comment

--- product.rb
+++ product.rb
@@ -12,21 +12,28 @@ class Product
     @name
   end
 
+  def base_price
+    @base_price
+  end
+
   def calculate_total
     price = @base_price
     tax_amount = @tax_rate * @base_price
     total = price  + tax_amount
     return total
   end
+
 end
 
-#test
-
-p1 = Product.new("apple", 50, 0.13)
-p2 = Product.new("pork", 70, 0.05)
-
-# puts p1
-# puts p2
-
-p p1
-p p2
+# #test
+#
+# p1 = Product.new("apple", 50, 0.13)
+# p2 = Product.new("pork", 70, 0.05)
+#
+# # puts p1
+# # puts p2
+#
+# p p1
+# p p2
+#
+# puts p1.calculate_total
